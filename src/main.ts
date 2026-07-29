bff5940100db3534f276225bc2fa21b387b12db4
Kristo <christo.losier@gmail.com>
2026-07-29 13:23:57 +0200

    feat: load phase 4 styles

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { startApplication } from './app/bootstrap';
 import './styles/index.css';
 import './styles/project-card.css';
+import './styles/catalogue.css';
+import './styles/project-detail.css';
 
 startApplication(document.querySelector<HTMLElement>('#app'));
