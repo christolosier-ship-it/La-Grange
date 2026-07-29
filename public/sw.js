7bbb4b0e6bbc05cfa25177bae3ab9a9286027512
Kristo <christo.losier@gmail.com>
2026-07-29 14:51:43 +0200

    chore(pwa): renew shell cache for phase 4 fixes

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 /* La Grange shell service worker: application data remains managed by IndexedDB. */
 const CACHE_PREFIX = 'la-grange-';
-const CACHE_NAME = `${CACHE_PREFIX}shell-v7`;
+const CACHE_NAME = `${CACHE_PREFIX}shell-v8`;
 const SCOPE_URL = new URL(self.registration.scope);
 const BASE_URL = SCOPE_URL.href;
 const SHELL_URLS = [
